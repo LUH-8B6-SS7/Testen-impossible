e2171043e2c6f24a44766c90035bffd5a6e3b498
LUH-8B6-SS7 <florian.wenzel@stud.uni-hannover.de>
2025-12-30 21:32:46 +0100

    Hinzufügen von der Kursliste vom Dozenten

---
+++ courses.js
@@ -0,0 +1,32 @@
+function addNewCourse() {
+  document.getElementById('myModal').style.display = "block";
+  document.getElementById("courses").classList.add("blurred");
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+  document.getElementById("courses").classList.remove("blurred");
+}
+
+function addCourse() {
+  var courseName = document.getElementById('newCourseName').value;
+  if(courseName) {
+    var table = document.querySelector("#courses table");
+    var newRow = table.insertRow();
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    cell1.innerHTML = `<a href="student.html">${courseName}</a>`;
+    cell2.innerHTML = "0"; // Default Teilnehmerzahl
+    document.getElementById('newCourseName').value = '';
+    closeModal();
+  } else {
+    alert('Bitte geben Sie einen Kursnamen ein.');
+  }
+}
+
+document.querySelector('#newCourseName').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      addCourse();
+    }
+});
+
