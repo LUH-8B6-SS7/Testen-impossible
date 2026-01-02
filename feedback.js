e924586838a934ec2de7850edd33838348d120b2
Fabian <fabian.kasten@stud.uni-hannover.de>
2026-01-02 20:41:40 +0100

    Feedbacktool fertig

--- feedback.js
+++ feedback.js
@@ -9,14 +9,17 @@ function unblock() {
     document.getElementById('flex-container').classList.remove("blocked");
     document.getElementById('foot').classList.remove("blocked");
 }
-
+    
+//Counter
+var id = 3;
 
 //Feedback Hinzufügen
 function addfeedback() {
     document.getElementById('addfb').style.display = "flex";
     toblock();
 }
-function closefeedback() {
+
+function cancelfeedback() {
     document.getElementById('addfb').style.display = "none";
     document.getElementById("feedbackName").value = "";
     document.getElementById("feedbackcontent").value = "";
@@ -26,30 +29,52 @@ function closefeedback() {
 function makefeedback(){
     var divElement = document.createElement("Div");
     divElement.setAttribute('class', 'post');
-    divElement.onclick = showFeedback;
+    var temp = id+'.1';
+    var temp2 = id+'.2';
+    divElement.addEventListener('click', function() {showFeedback(temp, temp2);});
 
     var divElement2 = document.createElement("Div");
     divElement2.setAttribute('class', 'container');
     var paragraph = document.createElement("P");
+    paragraph.id = id+'.1';
     var name = document.getElementById("feedbackName").value;
     if(!name){
         alert('Bitte geben sie eine Überschrift ein');
         document.getElementById("feedbackName").style = "border: 4px solid red";
     } else {
         document.getElementById("feedbackName").style = "border: 1px solid black";
-        var text = document.createTextNode(name)
+        var text = document.createTextNode(name);
+        var inhalt = document.getElementById("feedbackcontent").value;
+        var text2 = document.createTextNode(inhalt);
+        var cparagraph = document.createElement("P");
+        cparagraph.id = id+'.2';
+        cparagraph.setAttribute('class', 'content');
         
         paragraph.appendChild(text);
+        cparagraph.appendChild(text2);
         divElement2.appendChild(paragraph);
+        divElement2.appendChild(cparagraph);
         divElement.appendChild(divElement2);
 
         document.getElementById('flex-container').appendChild(divElement);
-        closefeedback();
+        id = id + 1;
+        cancelfeedback();
     }
 }
 
 //Feedback Lesen
-function showFeedback() {
-    toblock();
+function closefeedback() {
+    document.getElementById('fb').style.display = "none";
     unblock();
+}
+
+function showFeedback(value1, value2) {
+    document.getElementById('fb').style.display = "flex";
+    toblock();
+    
+    document.getElementById('popupheader').innerHTML = document.getElementById(value1).innerHTML;
+    document.getElementById('popupheader').style.display = "flex";
+
+    document.getElementById('popupcontent').innerHTML = document.getElementById(value2).innerHTML;
+    document.getElementById('popupcontent').style.display = "flex";
 }
