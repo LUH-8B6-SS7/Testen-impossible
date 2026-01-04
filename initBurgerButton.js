d5afbe893fddf0f1d94ffa2cd7be7453bbfb2b58
laura <laura.helene.gablenz@stud.uni-hannover.de>
2026-01-04 10:35:55 +0100

    burger zu feedback hinzugefügt

--- initBurgerButton.js
+++ initBurgerButton.js
@@ -9,3 +9,16 @@ burgerBtn.addEventListener('click', (e) => {
 document.addEventListener('click', () => {
     burgerMenu.classList.remove('open');
 });
+
+function toggleMenu() { //Skript für das wechseln des Bildes für das Burger-Menu
+    		const menu = document.getElementById('burgermenu');
+    		const image = document.getElementById('burger').firstElementChild;
+
+    		if (menu.style.display === "none" || menu.style.display === "") {
+        		menu.style.display = "block";
+        		image.src = "menu-icon-closed.png";
+    		} else {
+        		menu.style.display = "none";
+        		image.src = "menu-icon-open.png";
+    		}
+		}
